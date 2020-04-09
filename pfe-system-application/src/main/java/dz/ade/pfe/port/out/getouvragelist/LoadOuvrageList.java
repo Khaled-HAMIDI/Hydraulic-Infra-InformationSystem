2e6bf6383f5d8d0173fc514883343af69e15e955
rafiko666 <fr_tchoketch_kebir@esi.dz>
2020-04-09 15:38:56 +0100

    add ouvrage get list query

---
+++ pfe-system-application/src/main/java/dz/ade/pfe/port/out/getouvragelist/LoadOuvrageList.java
@@ -0,0 +1,8 @@
+package dz.ade.pfe.port.out.getouvragelist;
+
+import dz.ade.pfe.domain.ouvrage.Ouvrage;
+import java.util.List;
+
+public interface LoadOuvrageList {
+    List<Ouvrage> loadOuvrageList();
+}
