53b4ee1ba39e8f61f8fca40af95eabf2cb279d9f
Khaled <fk_hamidi@esi.dz>
2020-06-14 13:42:32 +0100

    Port out

---
+++ pfe-system-application/src/main/java/dz/ade/pfe/port/out/inventory/saveinventoryouvrage/SaveInventoryOuvrage.java
@@ -0,0 +1,7 @@
+package dz.ade.pfe.port.out.inventory.saveinventoryouvrage;
+
+import dz.ade.pfe.domain.ouvrage.InventoryOuvrage;
+
+public interface SaveInventoryOuvrage {
+    InventoryOuvrage saveInventoryOuvrage(InventoryOuvrage inventoryOuvrage);
+}
